930847b9a9c9beb7527b4bb8531d9889ebb1f681
IH Syed <ihsyedscc@gmail.com>
2023-05-17 21:25:33 +0500

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherinen Johnson" ></img>
+}
+
+export default function Fallery () {
+  return(
+    <div>
+      <h1>Amazing Nono</h1>
+      <br></br>
+      <div className="flex">
+      <Profile />
+       <Profile />
+        <Profile />
+         <Profile />
+      </div>
+    </div>
+  )
+}
